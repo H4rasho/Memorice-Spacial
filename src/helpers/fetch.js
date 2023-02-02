787ca28f3ecc2b3bab82d74315674e48315ab839
h4rasho <h4rasho@gmail.com>
2023-02-01 22:36:33 -0300

    migrate to vite

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,53 +1,42 @@
+const baseUrl = "http://localhost:4000/api";
 
-const baseUrl = process.env.REACT_APP_API_URL
+const fetchSinToken = (endpoint, data, method = "GET") => {
+  const url = `${baseUrl}/${endpoint}`;
 
- const fetchSinToken = (endpoint, data, method = 'GET') => {
+  if (method === "GET") {
+    return fetch(url);
+  } else {
+    return fetch(url, {
+      method,
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  }
+};
 
-    const url = `${ baseUrl }/${endpoint}`
+const fetchConToken = (endpoint, data, method = "GET") => {
+  const url = `${baseUrl}/${endpoint}`; //localhost:4000/api/endpoint
+  const token = localStorage.getItem("token") || "";
 
-    if (method === 'GET') {
-        return fetch(url);
-    }else {
-        return fetch(url, {
-            method,
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-    }
+  if (method === "GET") {
+    return fetch(url, {
+      method,
+      headers: {
+        "x-token": token,
+      },
+    });
+  } else {
+    return fetch(url, {
+      method,
+      headers: {
+        "Content-type": "application/json",
+        "x-token": token,
+      },
+      body: JSON.stringify(data),
+    });
+  }
+};
 
-}
-
-const fetchConToken = (endpoint, data, method='GET') =>{
- 
-    const url = `${ baseUrl }/${endpoint}`;//localhost:4000/api/endpoint
-    const token = localStorage.getItem('token') || '';
- 
-    if(method ==='GET'){
-        return fetch(url, {
-            method,
-            headers: {
-                'x-token': token
-            }
-        });
-    }else{
-        return fetch(url, {
-            method,
-            headers: {
-                'Content-type': 'application/json',
-                'x-token': token
-            },
-            body: JSON.stringify( data )
-        } );
-    }
- 
-}
-
-
-
-
-export {
-    fetchSinToken,
-    fetchConToken
-}
+export { fetchSinToken, fetchConToken };
